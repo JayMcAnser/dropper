7dbcf894e310cc23b6c474e02749f0867c27bb82
Jay McAnser <jay@mcanser.com>
2021-02-05 17:49:08 +0100

    fix no api

--- rebuild.sh
+++ rebuild.sh
@@ -2,10 +2,19 @@
 # updating import to the latest staging version
 # the testing more
 
+echo "retrieving new version"
 git stash
 git pull
 npm update
+echo "building site"
 cd site
+npm update
 npm run build
+echo "updating api"
+
+cd ../api
+npm update
 cd ..
+
+echo "restart pm2"
 pm2 restart curator
