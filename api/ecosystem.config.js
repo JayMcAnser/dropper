bec34f6d119d12c82e4aa6261c4b7f6509b0d7e8
Jay McAnser <jay@mcanser.com>
2021-02-05 17:22:05 +0100

    dropserver name

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'DropperApi',
+      name: 'DropServer',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './index.js'
