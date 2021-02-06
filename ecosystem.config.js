c02e2ff835635de62d443100a0f5d9a8efb1986b
Jay McAnser <jay@mcanser.com>
2021-02-06 14:09:59 +0100

    relocate ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'DropServer',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
-      script: './index.js'
+      script: './api/index.js'
     }
   ]
 }
