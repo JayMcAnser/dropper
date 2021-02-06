a9b422a9840859ad73281c581334c139f191c2e0
Jay McAnser <jay@mcanser.com>
2021-02-06 12:49:56 +0100

    new rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -2,19 +2,24 @@
 # updating import to the latest staging version
 # the testing more
 
-echo "retrieving new version"
+echo ">>> retrieving new version"
 git stash
 git pull
 npm update
-echo "building site"
-cd site
-npm update --force
-npm run build
-echo "updating api"
 
+echo ">>> updating api"
 cd ../api
 npm update --force
 cd ..
 
-echo "restart pm2"
+echo ">>> building site"
+cd site
+npm update --force
+npm run build
+
+rm -rf ../dist
+mv dist ../dist
+cd ..
+
+echo ">>> restart pm2"
 pm2 restart DropServer
