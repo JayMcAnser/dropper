25fc15edfc308d1cdc6f9417a012274d5ce47a10
Jay McAnser <jay@mcanser.com>
2021-02-06 18:53:55 +0100

    longer script

--- update.sh
+++ update.sh
@@ -3,13 +3,28 @@
 # updating import to the latest version
 
 git stash
+
+echo ">>> pull repo"
 git pull
+
+echo ">>> pull submodule api"
+cd api/vendors
+git pull
+cd ../..
+echo ">>> pull submodule site"
 cd site/src/vendors
 git pull
 cd ../..
+
+echo ">>> build site"
 npm run build
+
+echo ">>> move site to production"
+
 rm -rf ../dist
 mv dist ../dist
 cd ..
+
+echo ">>> restart server"
 pm2 restart DropServer
 
